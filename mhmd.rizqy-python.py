436d8109b1a469179298465cf1c136b988ceb799
Mhmdrizqy <112367853+Mhmdrizqy@users.noreply.github.com>
2022-09-02 10:23:23 +0700

    Add files via upload

---
+++ mhmd.rizqy-python.py
@@ -0,0 +1,30 @@
+# Rumus Persegi Sisi * Sisi
+
+sisi1 = 10
+sisi2 = 10
+luas_p = sisi1 * sisi2
+
+print("luas persegi adalah",luas_p)
+
+# Rumus Segitiga A * T \ 2
+alas = 20
+tinggi = 20
+luas_s = alas * tinggi /2
+
+print("luas segitiga adalah")
+
+# Rumus jajar genjang Alas * Tinggi
+alas = 25
+tinggi = 20
+luas_j = alas * tinggi
+
+print("luas jajar genjang adalah", luas_j)
+
+# Rumus Lingkaran phi* r *
+phi = 22 /7
+r = 14
+luas_L = phi * r * r
+print("\ni,luas persegi")
+sisi = int (input("masukan nilai sisi:"))
+luas_p - sisi * sisi
+print ("Luas Persegi adalah", luas_p)
